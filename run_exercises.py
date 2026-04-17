0f77edc94dba27cadc99e69e8f200308f213b8fc
picassothe3rd <covarr.hector@gmail.com>
2026-04-17 16:09:32 +0000

    chore: all exercises completed

--- run_exercises.py
+++ run_exercises.py
@@ -1,5 +1,4 @@
-import portfolio.data
-import portfolio.report
+import portfolio
 if __name__ == "__main__":
     my_portfolio = portfolio.data.create_portfolio("Retirement")
     portfolio.report.print_report(my_portfolio)
